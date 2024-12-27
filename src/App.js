22ac2f62a22147c1e55aea685029c10596396ec4
alankaiko <alamdfs@hotmail.com>
2024-12-27 08:50:46 -0300

    subindo primeiros componentes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Banner from "./components/Banner";
+import Container from "./components/Container";
+
 function App() {
     return (
         <>
-            <h1>teste</h1>
-            <h2>aff magiia</h2>
-            <h2>aff maria</h2>
+            <Header/>
+            <Banner image='home'/>
+            <Container>
+                <h1>teste</h1>
+                <h2>aff magia</h2>
+                <h2>aff maria</h2>
+            </Container>
+
+            <Footer/>
         </>
     );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from './footer.module.css';
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <h2> Desenvolvimento teste React</h2>
+        </footer>
+    );
+}
+
+export default Footer;
